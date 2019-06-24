8990898835b6f80f6bbe9bd49bef8a1472d82678
Alisher Turganaliyev <turalisher@gmail.com>
2019-06-24 23:29:14 +0200

    added chose image on imageViewer

--- js/components/PhoneViewer.js
+++ js/components/PhoneViewer.js
@@ -1,9 +1,9 @@
 export default class PhoneViewer {
-    constructor(element,props) {
+    constructor(element, props) {
         this.element = element;
         this.props = props;
         this.state = {
-            currentImage : this.props.phone.images[0]
+            currentImage: this.props.phone.images[0]
         }
 
         this.render();
@@ -16,17 +16,34 @@ export default class PhoneViewer {
             }
 
             this.props.onBack();
-        })
+        });
+
+        this.element.addEventListener('click', (event) => {
+            const delegateTarget =
+                event.target.closest('[data-element="thumbnail"]');
+
+            if (!delegateTarget) {
+                return;
+            }
+
+            this.state = {
+                ...this.state,
+                currentImage: delegateTarget.src,
+            };
+
+            this.render();
+        });
     }
 
     render() {
-        const { phone }  = this.props;
+        const {phone} = this.props;
+        const {currentImage} = this.state;
         console.log(phone);
 
         this.element.innerHTML = `
             <div>
 
-                <img class="phone" src="${this.state.currentImage}">
+             <img class="phone" src="${currentImage}">
             
                 <button data-element="back-button">Back</button>
                 <button>Add to basket</button>
@@ -39,7 +56,10 @@ export default class PhoneViewer {
                 <ul class="phone-thumbs">
                   ${phone.images.map(imageUrl => `
                   <li>
-                    <img src="${imageUrl}">
+                    <img 
+                        src="${imageUrl}"
+                        data-element="thumbnail">
+                        
                   </li>
                   
                   `).join('')}
